c3e1fb76616872bc97761afd3efd6462921ce09d
thanhanz <daothanhanz95@gmail.com>
2026-08-21 23:34:06 +0700

    refactor(outbox): add IOutboxWriter interface for writing domain events

--- src/Haggly.Application/Common/Messaging/IOutboxProcessor.cs
+++ src/Haggly.Application/Common/Messaging/IOutboxProcessor.cs
@@ -1,14 +1,7 @@
-using Haggly.Domain.Common.Events.V1;
-
 namespace Haggly.Application.Common.Messaging;
 
 public interface IOutboxProcessor
 {
-    Task WriteAsync<TEvent>(
-        TEvent domainEvent,
-        CancellationToken cancellationToken = default)
-        where TEvent : class, IDomainEvent;
-
     Task<int> ProcessPendingAsync(
         int batchSize = 100,
         CancellationToken cancellationToken = default);
