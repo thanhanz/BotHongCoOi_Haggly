dbb29141fa7d3d0268f2735e657ae928011e0ea4
thanhanz <daothanhanz95@gmail.com>
2026-08-25 15:54:11 +0700

    chore(payments): remove error queues for finance, inventory, and order payment succeeded

--- src/Haggly.Infrastructure/Messaging/PaymentMessagingNames.cs
+++ src/Haggly.Infrastructure/Messaging/PaymentMessagingNames.cs
@@ -10,13 +10,10 @@ public static class PaymentMessagingNames
     public const string PaymentRequestedErrorQueue = PaymentRequestedQueue + "_error";
 
     public const string FinancePaymentSucceededQueue = "finance-payment-succeeded-v1";
-    public const string FinancePaymentSucceededErrorQueue = FinancePaymentSucceededQueue + "_error";
 
     public const string InventoryPaymentSucceededQueue = "inventory-payment-succeeded-v1";
-    public const string InventoryPaymentSucceededErrorQueue = InventoryPaymentSucceededQueue + "_error";
 
     public const string OrderPaymentSucceededQueue = "order-payment-succeeded-v1";
-    public const string OrderPaymentSucceededErrorQueue = OrderPaymentSucceededQueue + "_error";
 
     public const string PaymentProcessingFaultsQueue = "payment-processing-faults-v1";
 }
